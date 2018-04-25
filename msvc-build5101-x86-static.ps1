05a3245f28edfe736e334b0954353034bf2f4391
Alexander Yalov <alexander.yalov@gmail.com>
2018-04-25 15:54:32 +0300

    delete MinGW

--- msvc-build5101-x86-static.ps1
+++ msvc-build5101-x86-static.ps1
@@ -1,30 +1,30 @@
-# 1. Start Visual Studio x64 Native Tools command line.
+# 1. Start Visual Studio x32 Native Tools command line.
 # 2. Run powershell.exe from Native Tools cmd.
 # 3. cd to path of qt5-minimalistic-builds repo.
 
-$version_base = "5.9"
-$version = "5.9.4"
+$version_base = "5.10"
+$version = "5.10.1"
 
-$qt_sources_url = "https://download.qt.io/official_releases/qt/" + $version_base + "/" + $version + "/single/qt-everywhere-opensource-src-" + $version + ".zip"
+$qt_sources_url = "https://download.qt.io/official_releases/qt/" + $version_base + "/" + $version + "/single/qt-everywhere-src-" + $version + ".zip"
 $qt_archive_file = $pwd.Path + "\qt.zip"
-$qt_src_base_folder = $pwd.Path + "\qt-everywhere-opensource-src-" + $version
+$qt_src_base_folder = $pwd.Path + "\qt-everywhere-src-" + $version
 
 $tools_folder = $pwd.Path + "\tools\"
 $type = "static-ltcg"
-$prefix_base_folder = "qt-" + $version + "-" + $type + "-msvc2017-x86_64"
+$prefix_base_folder = "qt-" + $version + "-" + $type + "-msvc2017-x86"
 $prefix_folder = $pwd.Path + "\" + $prefix_base_folder
 $build_folder = $pwd.Path + "\qtbuild"
 
 # OpenSSL
 # https://www.npcglib.org/~stathis/blog/precompiled-openssl/
 # 1.0.2l
-$openssl_base_folder = "C:\openssl"
-$openssl_include_folder = $openssl_base_folder + "\include64"
-$openssl_libs_folder = $openssl_base_folder + "\lib64"
+$openssl_base_folder = "c:\Qt\openssl-1.0.2l-vs2017"
+$openssl_include_folder = $openssl_base_folder + "\include"
+$openssl_libs_folder = $openssl_base_folder + "\lib"
 
 # Download Qt sources, unpack.
-Invoke-WebRequest -Uri $qt_sources_url -OutFile $qt_archive_file
-& "$tools_folder\7za.exe" x $qt_archive_file
+#Invoke-WebRequest -Uri $qt_sources_url -OutFile $qt_archive_file
+#& "$tools_folder\7za.exe" x $qt_archive_file
 
 # Configure.
 mkdir $build_folder
@@ -41,7 +41,7 @@ cp "$tools_folder\qtbinpatcher.*" "$prefix_folder\bin\"
 cp "$openssl_libs_folder\*eay32MT.lib" "$prefix_folder\lib\"
 
 # Fixup OpenSSL DLL paths.
-gci -r -include "*.prl" $prefix_folder | foreach-object { $a = $_.fullname; ( get-content $a ) | foreach-object { $_ -replace "C:\\\\openssl\\\\lib64\\\\", '$$$$[QT_INSTALL_LIBS]\\' } | set-content $a }
+gci -r -include "*.prl" $prefix_folder | foreach-object { $a = $_.fullname; ( get-content $a ) | foreach-object { $_ -replace "C:\\\\Qt\\\\openssl-1.0.2l-vs2017\\\\lib\\\\", '$$$$[QT_INSTALL_LIBS]\\' } | set-content $a }
 
 # Create final archive.
 & "$tools_folder\7za.exe" a -t7z "$prefix_base_folder.7z" "$prefix_folder" -mmt -mx9
